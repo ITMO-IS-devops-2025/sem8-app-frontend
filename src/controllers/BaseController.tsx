eff92accc0d76d3ec7aa64bbba0abaa138ccaede
Schipunova Ekaterina <89860556+schtschee@users.noreply.github.com>
2024-11-18 19:57:11 +0300

    Update BaseController.tsx

--- src/controllers/BaseController.tsx
+++ src/controllers/BaseController.tsx
@@ -9,8 +9,12 @@ export class ErrorResponse {
 export class BaseController {
 
     async api<T>(url: string, body: any = null, method: string = "GET"): Promise<T | ErrorResponse> {
-        let response = await this.request("/api/v1/" + url, body, method)
-        return response.ok ? JSON.parse(await response.text()) as T :
+        let response = await this.request("http://localhost:8080/api/v1/" + url, body, method)
+        let text = await response.text();
+        if (text == ""){
+            text = "{}"
+        }
+        return response.ok ? JSON.parse(text) as T :
             new ErrorResponse(response.status, await response.text())
     }
 
